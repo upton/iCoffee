561cefb8b7d3b92226827d2db093f6255ae27316
zehong.chen <zehong.chen@dmall.com>
2016-04-08 10:33:22 +0800

    add some test

---
+++ iCoffee-zest/src/main/java/name/upton/zest/threadlocal/Main.java
@@ -0,0 +1,10 @@
+package name.upton.zest.threadlocal;
+
+public class Main {
+    private static ThreadLocal<String> holder = new ThreadLocal<>();
+    
+    public static void main(String[] args) {
+        
+    }
+
+}
